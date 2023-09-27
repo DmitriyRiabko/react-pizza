aeb16ecc557e9b5b1ba551dee798dfbd77778345
vsegda_vinovat <riabko97@gmail.com>
2023-09-27 12:04:07 +0300

    adden new componets

---
+++ src/components/Categories.jsx
@@ -0,0 +1,17 @@
+const Categories = () => {
+    return (
+      <div class="categories">
+                <ul>
+                  <li class="active">ALL</li>
+                  <li>Meat</li>
+                  <li>Vegetarian</li>
+                  <li>Grill</li>
+                  <li>Spicy</li>
+                  <li>Closed</li>
+                </ul>
+              </div>
+    );
+  }
+
+
+export default Categories
